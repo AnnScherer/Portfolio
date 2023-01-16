d910065936cbe1c0dc3cda978b8099e5b144f559
AnnScherer <ann.hardt@gmx.de>
2023-01-16 11:38:53 +0100

    dfdr

--- src/components/routes.js
+++ src/components/routes.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Lebenslauf from "./pages/Lebenslauf";
 import NotFound from "./NotFound";
 import Impressum from "./pages/Impressum";
+import { Navigate } from "react-router-dom";
 
 const routes = [
   { path: "/", element: <Home />, id: 1 },
@@ -12,6 +13,7 @@ const routes = [
   { path: "/lebenslauf", element: <Lebenslauf />, id: 4 },
   { path: "/about-me", element: <AboutMe />, id: 5 },
   { path: "/impressum", element: <Impressum />, id: 6 },
+  { path: "/final-project-react", element: <Navigate to="/" />, id: 7 },
 ];
 
 export default routes;
